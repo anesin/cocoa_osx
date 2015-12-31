c050423ffe112d2c94bac24aa599dda0323e91fe
anesin <anesiner@gmail.com>
2015-12-31 21:00:34 +0900

    09 NSArrayController - RaiseMan - RaiseMan by Length

---
+++ 09 NSArrayController/RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by anesin on 2015. 12. 13..
+//  Copyright © 2015년 anesin. All rights reserved.
+//
+
+import Foundation
+
+class Employee : NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
