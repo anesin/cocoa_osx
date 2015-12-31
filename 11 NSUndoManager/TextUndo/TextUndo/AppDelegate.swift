8e249e1794a0ec5a65c1ac3d157f4eefa110856b
anesin <anesiner@gmail.com>
2015-12-31 21:02:04 +0900

    11 NSUndoManager - RaiseMan - TextUndo

---
+++ 11 NSUndoManager/TextUndo/TextUndo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextUndo
+//
+//  Created by anesin on 2015. 12. 27..
+//  Copyright © 2015년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
