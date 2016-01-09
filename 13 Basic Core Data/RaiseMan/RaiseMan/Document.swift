174ef56b0d24d0b6220aa772943cedb1290dbe4b
anesin <anesiner@gmail.com>
2016-01-09 20:24:51 +0900

    13 Basic Core Data - Challenge: Implement RaiseMan using Core Data

---
+++ 13 Basic Core Data/RaiseMan/RaiseMan/Document.swift
@@ -0,0 +1,33 @@
+//
+//  Document.swift
+//  RaiseMan
+//
+//  Created by anesin on 2016. 1. 9..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override func windowControllerDidLoadNib(aController: NSWindowController) {
+        super.windowControllerDidLoadNib(aController)
+        // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override var windowNibName: String? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return "Document"
+    }
+
+}
