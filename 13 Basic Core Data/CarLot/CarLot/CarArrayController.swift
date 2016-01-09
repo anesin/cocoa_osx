8b252cf55639a3aa1ff6ab7dcb3f7695f76f6432
anesin <anesiner@gmail.com>
2016-01-09 19:14:40 +0900

    13 Basic Core Data - CarLot

---
+++ 13 Basic Core Data/CarLot/CarLot/CarArrayController.swift
@@ -0,0 +1,13 @@
+//
+//  CarArrayController.swift
+//  CarLot
+//
+//  Created by anesin on 2016. 1. 9..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+class CarArrayController: NSArrayController {
+
+}
