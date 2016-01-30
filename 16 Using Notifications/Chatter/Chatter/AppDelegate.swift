da70f95ede6c55a16c3cf28ea889216866b86418
anesin <anesiner@gmail.com>
2016-01-30 18:26:37 +0900

    16 Using Notifications - Chatter

---
+++ 16 Using Notifications/Chatter/Chatter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Chatter
+//
+//  Created by anesin on 2016. 1. 30..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 16 Using Notifications/Chatter/Chatter/ChatWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatWindowController.swift
+//  Chatter
+//
+//  Created by anesin on 2016. 1. 30..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+class ChatWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
