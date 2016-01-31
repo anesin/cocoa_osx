fa210884be9a1eec45d8da2f76c216a032fd2df4
anesin <anesiner@gmail.com>
2016-01-31 14:50:13 +0900

    16 Using Notifications - Challenge: Beep-beep!

---
+++ 16 Using Notifications/Chatter beep/Chatter/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  Chatter
+//
+//  Created by anesin on 2016. 1. 30..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var windowControllers: [ChatWindowController] = []
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        addWindowController()
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func displayNewWindow(sender: NSMenuItem) {
+        addWindowController()
+    }
+
+    // MARK: - Helpers
+    
+    func addWindowController() {
+        let windowController = ChatWindowController()
+        windowController.showWindow(self)
+        windowControllers.append(windowController)
+    }
+    
+    // MARK: - Notifications
+    
+    // NSApplicationDidResignActiveNotification
+    func applicationDidResignActive(notification: NSNotification) {
+        NSBeep()
+    }
+
+}
+
