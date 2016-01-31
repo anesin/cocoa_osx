0df5edee1d74260f5aed313fd92346a5a802aca8
anesin <anesiner@gmail.com>
2016-01-31 15:30:16 +0900

    16 Using Notifications - Challenge: Add Usernames

---
+++ 16 Using Notifications/Chatter username/Chatter/ChatWindowController.swift
@@ -0,0 +1,80 @@
+//
+//  ChatWindowController.swift
+//  Chatter
+//
+//  Created by anesin on 2016. 1. 30..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+private let ChatWindowControllerDidSendMessageNotification = "com.wordpress.anesiner.chatter.ChatWindowControllerDidSendMessageNotification"
+private let ChatWindowControllerMessageKey = "com.wordpress.anesiner.chatter.ChatWindowControllerMessageKey"
+
+class ChatWindowController: NSWindowController {
+    
+    dynamic var log: NSAttributedString = NSAttributedString(string: "")
+    dynamic var message: String?
+    
+    // NSTextView does not support weak references.
+    @IBOutlet var textView: NSTextView!
+    
+    var username: String = "anonymous"
+    @IBOutlet weak var name: NSTextField!
+
+    // MARK: - Lifecycle
+    
+    override var windowNibName: String {
+        return "ChatWindowController"
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: Selector("receiveDidSendMessageNotification:"), name: ChatWindowControllerDidSendMessageNotification, object: nil)
+    }
+    
+    deinit {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func send(sender: NSButton) {
+        sender.window?.endEditingFor(nil)
+        if let message = message where message.characters.count > 0 {
+            let name = self.name.stringValue
+            if name.characters.count > 0 && username != name {
+                username = name
+            }
+            let userInfo = [ChatWindowControllerMessageKey: username + " - " + message]
+            let notificationCenter = NSNotificationCenter.defaultCenter()
+            notificationCenter.postNotificationName(ChatWindowControllerDidSendMessageNotification, object: self, userInfo: userInfo)
+        }
+        message = ""
+    }
+    
+    // MARK: - Notifications
+    
+    // ChatWindowControllerDidSendMessageNotification
+    func receiveDidSendMessageNotification(note: NSNotification) {
+        let mutableLog = log.mutableCopy() as! NSMutableAttributedString
+        
+        if log.length > 0 {
+            mutableLog.appendAttributedString(NSAttributedString(string: "\n"))
+        }
+        
+        let userInfo = note.userInfo! as! [String: String]
+        let message = userInfo[ChatWindowControllerMessageKey]!
+        
+        let logLine = NSAttributedString(string: message)
+        mutableLog.appendAttributedString(logLine)
+        
+        log = mutableLog.copy() as! NSAttributedString
+        
+        textView.scrollRangeToVisible(NSRange(location: log.length, length: 0))
+    }
+    
+}
