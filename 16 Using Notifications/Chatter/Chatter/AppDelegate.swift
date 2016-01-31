ecc4097e9020ab9873fd64876db86a0a1f278860
anesin <anesiner@gmail.com>
2016-01-31 14:07:38 +0900

    16 Using Notifications - Chatter

--- 16 Using Notifications/Chatter/Chatter/AppDelegate.swift
+++ 16 Using Notifications/Chatter/Chatter/AppDelegate.swift
@@ -10,18 +10,30 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
+    
+    var windowControllers: [ChatWindowController] = []
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        addWindowController()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
+    
+    // MARK: - Actions
+    
+    @IBAction func displayNewWindow(sender: NSMenuItem) {
+        addWindowController()
+    }
 
+    // MARK: - Helpers
+    
+    func addWindowController() {
+        let windowController = ChatWindowController()
+        windowController.showWindow(self)
+        windowControllers.append(windowController)
+    }
 
 }
 
--- 16 Using Notifications/Chatter/Chatter/ChatWindowController.swift
+++ 16 Using Notifications/Chatter/Chatter/ChatWindowController.swift
@@ -8,12 +8,66 @@
 
 import Cocoa
 
-class ChatWindowController: NSWindowController {
+private let ChatWindowControllerDidSendMessageNotification = "com.wordpress.anesiner.chatter.ChatWindowControllerDidSendMessageNotification"
+private let ChatWindowControllerMessageKey = "com.wordpress.anesiner.chatter.ChatWindowControllerMessageKey"
 
+class ChatWindowController: NSWindowController {
+    
+    dynamic var log: NSAttributedString = NSAttributedString(string: "")
+    dynamic var message: String?
+    
+    // NSTextView does not support weak references.
+    @IBOutlet var textView: NSTextView!
+
+    // MARK: - Lifecycle
+    
+    override var windowNibName: String {
+        return "ChatWindowController"
+    }
+    
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: Selector("receiveDidSendMessageNotification:"), name: ChatWindowControllerDidSendMessageNotification, object: nil)
+    }
+    
+    deinit {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func send(sender: NSButton) {
+        sender.window?.endEditingFor(nil)
+        if let message = message {
+            let userInfo = [ChatWindowControllerMessageKey: message]
+            let notificationCenter = NSNotificationCenter.defaultCenter()
+            notificationCenter.postNotificationName(ChatWindowControllerDidSendMessageNotification, object: self, userInfo: userInfo)
+        }
+        message = ""
+    }
+    
+    // MARK: - Notifications
+    
+    // ChatWindowControllerDidSendMessageNotification
+    func receiveDidSendMessageNotification(note: NSNotification) {
+        let mutableLog = log.mutableCopy() as! NSMutableAttributedString
+        
+        if log.length > 0 {
+            mutableLog.appendAttributedString(NSAttributedString(string: "\n"))
+        }
+        
+        let userInfo = note.userInfo! as! [String: String]
+        let message = userInfo[ChatWindowControllerMessageKey]!
+        
+        let logLine = NSAttributedString(string: message)
+        mutableLog.appendAttributedString(logLine)
+        
+        log = mutableLog.copy() as! NSAttributedString
+        
+        textView.scrollRangeToVisible(NSRange(location: log.length, length: 0))
     }
     
 }
