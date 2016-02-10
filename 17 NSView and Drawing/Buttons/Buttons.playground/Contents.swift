f04c8e8874f7ba9ec1f2a070d010d6db364942fc
anesin <anesiner@gmail.com>
2016-02-10 19:01:18 +0900

    17 NSView and Drawing - Buttons.playground

---
+++ 17 NSView and Drawing/Buttons/Buttons.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let frame = NSRect(x: 0, y: 0, width: 100, height: 100)
+let button = NSButton(frame: frame)
+button.bezelStyle = .RoundedBezelStyle
+button.title = "I, Button"
+button
