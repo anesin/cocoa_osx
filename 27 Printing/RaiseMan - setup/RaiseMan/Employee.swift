e9cae681479db6bd048c9fd3299158233b9b2967
anesin <anesiner@gmail.com>
2016-03-19 14:28:03 +0900

    27 Printing - Challenge: Persist Page Setup

---
+++ 27 Printing/RaiseMan - setup/RaiseMan/Employee.swift
@@ -0,0 +1,47 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by anesin on 2015. 12. 13..
+//  Copyright © 2015년 anesin. All rights reserved.
+//
+
+import Foundation
+
+class Employee : NSObject, NSCoding {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+    
+    override init() {
+        super.init()
+    }
+    
+    func validateRaise(raiseNumberPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>, error outError: NSErrorPointer) -> Bool {
+        let raiseNumber = raiseNumberPointer.memory
+        if raiseNumber == nil {
+            let domain = "UserInputValidationErrorDomain"
+            let code = 0
+            let userInfo = [NSLocalizedDescriptionKey: "An employee's raise must be a number."]
+            outError.memory = NSError(domain: domain, code: code, userInfo: userInfo)
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    // MARK: - NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        if let name = name {
+            aCoder.encodeObject(name, forKey: "name")
+        }
+        aCoder.encodeFloat(raise, forKey: "raise")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("name") as! String?
+        raise = aDecoder.decodeFloatForKey("raise")
+        super.init()
+    }
+    
+}
