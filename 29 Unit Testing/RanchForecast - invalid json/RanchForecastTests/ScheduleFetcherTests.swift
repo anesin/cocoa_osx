b3daf51090d0eb88752ed1b7fcec92bef182da9d
anesin <anesiner@gmail.com>
2016-04-02 21:25:45 +0900

    29 Unit Testing - Challenge: Test Invalid JSON Dictionary

---
+++ 29 Unit Testing/RanchForecast - invalid json/RanchForecastTests/ScheduleFetcherTests.swift
@@ -0,0 +1,61 @@
+//
+//  ScheduleFetcherTests.swift
+//  RanchForecast
+//
+//  Created by anesin on 2016. 4. 2..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import XCTest
+import RanchForecast
+
+class ScheduleFetcherTests: XCTestCase {
+
+    var fetcher: ScheduleFetcher!
+    
+    override func setUp() {
+        super.setUp()
+        fetcher = ScheduleFetcher()
+    }
+    
+    override func tearDown() {
+        fetcher = nil
+        super.tearDown()
+    }
+    
+    func testCreateCourseFromValidDictionary() {
+        let course: Course! = fetcher.courseFromDictionary(Constants.validCourseDict)
+        
+        XCTAssertNotNil(course)
+        XCTAssertEqual(course.title, Constants.title)
+        XCTAssertEqual(course.url, Constants.url)
+        XCTAssertEqual(course.nextStartDate, Constants.date)
+    }
+    
+    func testCreateCourseFromInalidDictionary() {
+        let invalidCourseDict = NSMutableDictionary(dictionary: Constants.validCourseDict)
+
+        for (key, value) in invalidCourseDict {
+            invalidCourseDict.removeObjectForKey(key)
+            let course: Course! = fetcher.courseFromDictionary(invalidCourseDict)
+            XCTAssertNil(course)
+            invalidCourseDict[key as! String] = value
+        }
+    }
+    
+    func testResultFromValidHTTPResponseAndValidData() {
+        let result = fetcher.resultFromData(Constants.jsonData, response: Constants.okResponse, error: nil)
+        
+        switch result {
+        case .Success(let courses):
+            XCTAssert(courses.count == 1)
+            let theCourse = courses[0]
+            XCTAssertEqual(theCourse.title, Constants.title)
+            XCTAssertEqual(theCourse.url, Constants.url)
+            XCTAssertEqual(theCourse.nextStartDate, Constants.date)
+        default:
+            XCTFail("Result contains Failure, but Success was expected.")
+        }
+    }
+
+}
