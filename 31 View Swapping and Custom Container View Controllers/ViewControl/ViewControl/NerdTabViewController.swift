99b32952faa4915a27696bd1ef39ece95ee8e619
anesin <anesiner@gmail.com>
2016-04-14 15:28:59 +0900

    31 View Swapping and Custom Container View Controllers - ViewControl

---
+++ 31 View Swapping and Custom Container View Controllers/ViewControl/ViewControl/NerdTabViewController.swift
@@ -0,0 +1,116 @@
+//
+//  NerdTabViewController.swift
+//  ViewControl
+//
+//  Created by anesin on 2016. 4. 14..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+@objc
+protocol ImageRepresentable {
+    var image: NSImage? { get }
+}
+
+class NerdTabViewController : NSViewController {
+    
+    var box = NSBox()
+    var buttons: [NSButton] = []
+    
+    func selectTabAtIndex(index: Int) {
+        assert(0 <= index && index < childViewControllers.count, "index out of range")
+        for (i, button) in buttons.enumerate() {
+            button.state = (index == i) ? NSOnState : NSOffState
+        }
+        let viewController = childViewControllers[index]
+        box.contentView = viewController.view
+    }
+    
+    func selectTab(sender: NSButton) {
+        let index = sender.tag
+        selectTabAtIndex(index)
+    }
+    
+    override func loadView() {
+        view = NSView()
+        reset()
+    }
+    
+    func reset() {
+        view.subviews = []
+        
+        let buttonWidth: CGFloat = 28
+        let buttonHeight: CGFloat = 28
+        
+        let viewControllers = childViewControllers
+        buttons = viewControllers.enumerate().map { (index, viewController) -> NSButton in
+            let button = NSButton()
+            button.setButtonType(.ToggleButton)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.bordered = false
+            button.target = self
+            button.action = #selector(NerdTabViewController.selectTab(_:))
+            button.tag = index
+//            button.image = NSImage(named: NSImageNameFlowViewTemplate)
+            if let viewController = viewController as? ImageRepresentable {
+                button.image = viewController.image
+            } else {
+                button.title = viewController.title!
+            }
+            button.addConstraints([
+                NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: buttonWidth),
+                NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: buttonHeight),
+            ])
+            return button
+        }
+        
+        let stackView = NSStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.orientation = .Horizontal
+        stackView.spacing = 4
+        for button in buttons {
+            stackView.addView(button, inGravity: .Center)
+        }
+        
+        box.translatesAutoresizingMaskIntoConstraints = false
+        box.borderType = .NoBorder
+        box.boxType = .Custom
+        
+        let separator = NSBox()
+        separator.boxType = .Separator
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.subviews = [stackView, separator, box]
+        
+        let views = ["stack": stackView, "separator": separator, "box": box]
+        let metrics = ["buttonHeight": buttonHeight]
+        
+        func addVisualFormatConstraints(visualFormat:String) {
+            view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: [], metrics: metrics, views: views))
+        }
+        addVisualFormatConstraints("H:|[stack]|")
+        addVisualFormatConstraints("H:|[separator]|")
+        addVisualFormatConstraints("H:|[box(>=100)]|")
+        addVisualFormatConstraints("V:|[stack(buttonHeight)][separator(==1)][box(>=100)]|")
+        
+        if childViewControllers.count > 0 {
+            selectTabAtIndex(0)
+        }
+    }
+    
+    override func insertChildViewController(childViewController: NSViewController, atIndex index: Int) {
+        super.insertChildViewController(childViewController, atIndex: index)
+        if viewLoaded {
+            reset()
+        }
+    }
+    
+    override func removeChildViewControllerAtIndex(index: Int) {
+        super.removeChildViewControllerAtIndex(index)
+        if viewLoaded {
+            reset()
+        }
+    }
+    
+}
