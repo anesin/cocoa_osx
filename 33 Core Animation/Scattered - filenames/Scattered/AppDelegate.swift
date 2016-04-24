6cb952bf222ef7fa34d20d2927c0ec1fb71f517c
anesin <anesiner@gmail.com>
2016-04-24 18:44:14 +0900

    33 Core Animation - Challenge: Show Filenames

---
+++ 33 Core Animation/Scattered - filenames/Scattered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scattered
+//
+//  Created by anesin on 2016. 4. 24..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
