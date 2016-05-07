0c0d04678f9cdb90def8acfccaae4053edecd942
anesin <anesiner@gmail.com>
2016-05-07 14:22:08 +0900

    35 NSTask - iPing

---
+++ 35 NSTask/iPing/iPing/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  iPing
+//
+//  Created by anesin on 2016. 5. 7..
+//  Copyright © 2016년 anesin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet var outputView: NSTextView!
+    @IBOutlet weak var hostField: NSTextField!
+    @IBOutlet weak var startButton: NSButton!
+    var task: NSTask?
+    var pipe: NSPipe?
+    var fileHandle: NSFileHandle?
+    
+    @IBAction func togglePinging(sender: NSButton) {
+        // Is there a running task?
+        if let task = task {
+            // If there is, stop it!
+            task.interrupt()
+        } else {
+            // If there isn't start one.
+            
+            // Create a new task
+            let task = NSTask()
+            task.launchPath = "/sbin/ping"
+            task.arguments = ["-c10", hostField.stringValue]
+            
+            // Create a new pipe for standardOutput
+            let pipe = NSPipe()
+            task.standardOutput = pipe
+            
+            // Grab the file handle
+            let fileHandle = pipe.fileHandleForReading
+            
+            self.task = task
+            self.pipe = pipe
+            self.fileHandle = fileHandle
+            
+            let notificationCenter = NSNotificationCenter.defaultCenter()
+            notificationCenter.removeObserver(self)
+            notificationCenter.addObserver(self, selector: #selector(ViewController.receiveDataReadyNotification(_:)), name: NSFileHandleReadCompletionNotification, object: fileHandle)
+            notificationCenter.addObserver(self, selector: #selector(ViewController.receiveTaskTerminatedNotification(_:)), name: NSTaskDidTerminateNotification, object: task)
+            task.launch()
+            
+            outputView.string = ""
+            
+            fileHandle.readInBackgroundAndNotify()
+        }
+    }
+    
+    func appendData(data: NSData) {
+        let string = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+        let textStorage = outputView.textStorage!
+        let endRange = NSRange(location: textStorage.length, length: 0)
+        textStorage.replaceCharactersInRange(endRange, withString: string)
+    }
+    
+    func receiveDataReadyNotification(notification: NSNotification) {
+        let data = notification.userInfo![NSFileHandleNotificationDataItem] as! NSData
+        let length = data.length
+        
+        print("received data: \(length) bytes")
+        if length > 0 {
+            self.appendData(data)
+        }
+        
+        // If the task is running, start reading again
+        if let fileHandle = fileHandle {
+            fileHandle.readInBackgroundAndNotify()
+        }
+    }
+    
+    func receiveTaskTerminatedNotification(notification: NSNotification) {
+        print("task terminated")
+        
+        task = nil
+        pipe = nil
+        fileHandle = nil
+        
+        startButton.state = 0
+    }
+
+}
+
